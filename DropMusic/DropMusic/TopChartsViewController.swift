383db70fa3b0adf2541b7acb0c0ebe5257d88fdf
Angel Contreras <angel.contreras23@me.com>
2016-10-12 04:49:12 -0600

    Setup the neccessary UI for the topchart view

---
+++ DropMusic/DropMusic/TopChartsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TopChartsViewController.swift
+//  DropMusic
+//
+//  Created by Angel Contreras on 10/12/16.
+//  Copyright © 2016 Angel Contreras. All rights reserved.
+//
+
+import UIKit
+
+class TopChartsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
