1abbce7c2aa084a708370edf0b3ff72077f525da
Angel Contreras <angel.contreras23@me.com>
2016-10-14 17:27:10 -0600

    need to populate the search controller with the itunes network call

--- DropMusic/DropMusic/TopChartsTableViewCell.swift
+++ DropMusic/DropMusic/TopChartsTableViewCell.swift
@@ -9,23 +9,16 @@
 import UIKit
 
 class TopChartsTableViewCell: UITableViewCell {
-    
-    
-    
     @IBOutlet weak var collectinImage: UIImageView!
     @IBOutlet weak var indexLabel: UILabel!
     @IBOutlet weak var songNameLabel: UILabel!
     @IBOutlet weak var artistNameLabel: UILabel!
-    
-    
-    
     
     func updateWith(song: Song, on index: Int){
         self.songNameLabel.text = song.songName
         self.artistNameLabel.text = song.artistSong
         self.indexLabel.text = "\(index + 1)"
         collectinImage.image = song.mediumImage
-    }
-    
+    } 
 }
 
